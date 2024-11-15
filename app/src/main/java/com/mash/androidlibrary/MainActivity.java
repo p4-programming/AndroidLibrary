76e2f26158309db645df024d5a5c5d50fbaa577e
deepak.kumar@mashvirtual.in <deepak.kumar@mashvirtual.in>
2024-11-15 18:00:49 +0530

    Initial commit

---
+++ app/src/main/java/com/mash/androidlibrary/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mash.androidlibrary;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mash.toastlib.Toasty;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toasty.simpleToast(this,"Deepak Kumar");
+
+    }
+}
